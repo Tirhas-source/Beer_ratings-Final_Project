e843ef291e8c0cd679e7d68d7a9aef3d3cf2314a
Tirhas-source <56926891+Tirhas-source@users.noreply.github.com>
2021-05-08 20:19:40 -0400

    Commit 2

--- beer_rating_files/app.py
+++ beer_rating_files/app.py
@@ -2,8 +2,36 @@ import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
 
+
+
+def return_prediction(model,scaler,sample_json):
+    
+    # For larger data features, you should probably write a for loop
+    # That builds out this array for you
+    
+    aroma= sample_json['review_aroma']
+    appearance = sample_json['review_appearance']
+    palate = sample_json['review_palate']
+    taste = sample_json['review_taste']
+    abv = sample_json['beer_abv']
+    
+    beer = [[aroma,appearance,palate,taste,abv]]
+    
+    scaled_test = scaler.transform(X_test)
+    
+    # classes = np.array(['setosa', 'versicolor', 'virginica'])
+    
+    class_ind = model.predict_classes(X_test)
+    
+    return classes[X_test][0]
+
+
+
+# REMEMBER TO LOAD THE MODEL AND THE SCALER!
 app = Flask(__name__)
-model = pickle.load(open('logmodel.pkl', 'rb'))
+beer_model = pickle.load(open('logmodel.pkl', 'rb'))
+beer_scaler = joblib.load("scaler.pkl")
+
 
 @app.route('/')
 def home():
